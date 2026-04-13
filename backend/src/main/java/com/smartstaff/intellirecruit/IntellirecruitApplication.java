3da85b1d12dacec57f7a4da65a90ae78cb0ffa2c
Usaid-Khan <usaid1521@gmail.com>
2026-04-13 17:47:55 +0500

    first commit

---
+++ backend/src/main/java/com/smartstaff/intellirecruit/IntellirecruitApplication.java
@@ -0,0 +1,13 @@
+package com.smartstaff.intellirecruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellirecruitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellirecruitApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/smartstaff/intellirecruit/IntellirecruitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartstaff.intellirecruit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellirecruitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
